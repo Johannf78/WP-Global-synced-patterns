736541547fce339ead5f9daae66272cb3f276cf1
Johann Fritz <35003699+Johannf78@users.noreply.github.com>
2024-10-19 09:35:37 +0200

    Update functions.php
            // Handle the case where the post is not found
            $post_content = "Post not found.";

--- functions.php
+++ functions.php
@@ -1,6 +1,4 @@
-//This function takes a post_id attribute. The post id is the Post id of the synced pattern in the master site.
-//Synced patterns are also saved in the wp_posts table.
-//One can then display this synced patter in all of the sites in a WordPress Network installation.
+
 function get_global_pattern($atts) {
     // Extract the attributes
     $atts = shortcode_atts(
@@ -12,7 +10,7 @@ function get_global_pattern($atts) {
     );
 	$post_id = $atts['post_id'];
     //For debuging. Use the attribute in your shortcode output
-    //$output = "The value of post_id is: " . $post_id ;
+    $output = "The value of post_id is: " . $post_id ;
 	
 	// Switch to the main site that contains all the master patterns.
     switch_to_blog(1); // Main site ID is usually 1
@@ -23,12 +21,18 @@ function get_global_pattern($atts) {
 	//switch back to the defualt blog.
     restore_current_blog();
 	
-	//Get just the post_content out of the array of returned values.
-	$post_content =  $post->post_content;
-		
+	// Check if the post is not null
+    if ($post !== null) {
+        // Get just the post_content out of the array of returned values.
+        $post_content = $post->post_content;
+    } else {
+        // Handle the case where the post is not found
+        $post_content = "Post not found.";
+    }	
+	
 	// Apply the 'the_content' filter - Let WP process all the WP code and output html.
     $output = apply_filters('the_content', $post_content);
-
+    
     return $output;
 }
 add_shortcode('global_pattern', 'get_global_pattern');
